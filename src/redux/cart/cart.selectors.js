fc52312da195838d90965e043b0d5cdad251f511
youssefhany96 <youssef96hany@gmail.com>
2022-01-30 10:24:26 +0200

    added checkput page and checkout item component

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -13,4 +13,19 @@ export const selectCartItemsCount = createSelector(
     (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
     0
   )  
-)
+);
+
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
+);
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems => 
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) => 
+        accumulatedQuantity + cartItem.quantity * cartItem.price,
+    0
+  )  
+); 
